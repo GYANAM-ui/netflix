1dc23737a2d05ff5600b262e7ff724011facc4b6
GYANAM <ggyanam869@gmail.com>
2022-03-22 15:26:50 +0530

    first commit

---
+++ components/card/section-cards.js
@@ -0,0 +1,32 @@
+import Card from "./card";
+import Link from "next/link";
+import clsx from "classnames";
+import styles from "./section-cards.module.css";
+
+const SectionCards = (props) => {
+  const { title, videos = [], size, shouldWrap = false, shouldScale } = props;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
+        {videos.map((video, idx) => {
+          return (
+            // eslint-disable-next-line react/jsx-key
+            <Link href={`/video/${video.id}`}>
+              <a>
+                <Card
+                  id={idx}
+                  imgUrl={video.imgUrl}
+                  size={size}
+                  shouldScale={shouldScale}
+                />
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default SectionCards;
---
+++ components/loading/loading.js
@@ -0,0 +1,11 @@
+import styles from "./loading.module.css";
+
+const Loading = () => {
+  return (
+      <center>
+          <p className={styles.loader}>Loading...</p>
+      </center>
+  );
+};
+
+export default Loading;
